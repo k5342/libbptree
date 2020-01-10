3ca466c315b03d17a55a371231a1eb01eb5b5247
k <k@ksswre.net>
2020-01-10 16:42:55 +0900

    tests/leaf_insert.c: use function for node

--- tests/leaf_insert.c
+++ tests/leaf_insert.c
@@ -67,36 +67,36 @@ bptree_test_result testcase_check_leaf_insert_divide_even(){
 	}
 
 	// check children
-	bptree_node_t *left_child = bptree_leaf_get_element_by_index(bpt, bpt->root, 0);
-	bptree_node_t *right_child = bptree_leaf_get_element_by_index(bpt, bpt->root, 1);
+	bptree_node_t *left_child = bptree_node_get_children_by_index(bpt, bpt->root, 0);
+	bptree_node_t *right_child = bptree_node_get_children_by_index(bpt, bpt->root, 1);
 	
-	if (bptree_leaf_get_key_count(bpt, left_child) != 6){
-		printf("left_child->used != 6 (expected 6, but %d)\n", bptree_leaf_get_key_count(bpt, left_child));
+	if (bptree_node_get_key_count(bpt, left_child) != 6){
+		printf("left_child->used != 6 (expected 6, but %d)\n", bptree_node_get_key_count(bpt, left_child));
 		return BPTREE_TEST_FAILED;
 	}
-	if (bptree_leaf_get_key_count(bpt, right_child) != 5){
-		printf("left_child->used != 5 (expected 5, but %d)\n", bptree_leaf_get_key_count(bpt, right_child));
+	if (bptree_node_get_key_count(bpt, right_child) != 5){
+		printf("left_child->used != 5 (expected 5, but %d)\n", bptree_node_get_key_count(bpt, right_child));
 		return BPTREE_TEST_FAILED;
 	}
 	
 	for (int i = 0; i < 6; i++){
-		if (bptree_leaf_get_key_by_index(bpt, left_child, i) != (bptree_key_t)i){
-			printf("left_child->keys[i] != i (expected %d, but %lld)\n", i, bptree_leaf_get_key_by_index(bpt, left_child, i));
+		if (bptree_node_get_key_by_index(bpt, left_child, i) != (bptree_key_t)i){
+			printf("left_child->keys[i] != i (expected %d, but %lld)\n", i, bptree_node_get_key_by_index(bpt, left_child, i));
 			return BPTREE_TEST_FAILED;
 		}
-		if (bptree_leaf_get_element_by_index(bpt, left_child, i) != (void *)i){
-			printf("left_child->children[i] != i (expected %p, but %p)\n", (void *)i, bptree_leaf_get_element_by_index(bpt, left_child, i));
+		if (bptree_node_get_children_by_index(bpt, left_child, i) != (void *)i){
+			printf("left_child->children[i] != i (expected %p, but %p)\n", (void *)i, bptree_node_get_children_by_index(bpt, left_child, i));
 			return BPTREE_TEST_FAILED;
 		}
 	}
 	
 	for (int i = 0; i < 5; i++){
-		if (bptree_leaf_get_key_by_index(bpt, right_child, i) != (bptree_key_t)(i + 6)){
-			printf("right_child->keys[i] != i (expected %d, but %lld)\n", (i + 6), bptree_leaf_get_key_by_index(bpt, right_child, i));
+		if (bptree_node_get_key_by_index(bpt, right_child, i) != (bptree_key_t)(i + 6)){
+			printf("right_child->keys[i] != i (expected %d, but %lld)\n", (i + 6), bptree_node_get_key_by_index(bpt, right_child, i));
 			return BPTREE_TEST_FAILED;
 		}
-		if (bptree_leaf_get_element_by_index(bpt, right_child, i) != (void *)(i + 6)){
-			printf("right_child->children[i] != i (expected %p, but %p)\n", (void *)(i + 6), bptree_leaf_get_element_by_index(bpt, right_child, i));
+		if (bptree_node_get_children_by_index(bpt, right_child, i) != (void *)(i + 6)){
+			printf("right_child->children[i] != i (expected %p, but %p)\n", (void *)(i + 6), bptree_node_get_children_by_index(bpt, right_child, i));
 			return BPTREE_TEST_FAILED;
 		}
 	}
