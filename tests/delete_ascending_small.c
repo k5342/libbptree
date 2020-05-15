2e2777c33ef26b0917c72841e9b2a043cb62cdef
k <k@ksswre.net>
2020-05-15 18:42:39 +0900

    tests/delete_*.c: fix test does not exit on fail

--- tests/delete_ascending_small.c
+++ tests/delete_ascending_small.c
@@ -7,8 +7,14 @@ int main(int argc, char *argv[]){
 	bptree_test_context *c = bptree_create_test_context(2, 200);
 	
 	insert_in_asc(c);
-	delete_in_asc(c);
+	if (delete_in_asc(c) != BPTREE_TEST_PASSED){
+		printf("delete test fail\n");
+		goto fail;
+	}
 	
+	bptree_destroy_test_context(c);
+	return 0;
+fail:
 	bptree_destroy_test_context(c);
 	return 0;
 }
